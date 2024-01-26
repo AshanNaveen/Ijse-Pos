fe322cc0004696c8e6eeee2b32a5f9676aa8c454
Ashan Naveen Theekshana <ashannvn@gmail.com>
2024-01-27 00:23:51 +0530

    Initial commit

---
+++ src/main/java/com/ijse/IjsePos/IjsePosApplication.java
@@ -0,0 +1,13 @@
+package com.ijse.IjsePos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjsePosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IjsePosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ijse/IjsePos/IjsePosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijse.IjsePos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjsePosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
